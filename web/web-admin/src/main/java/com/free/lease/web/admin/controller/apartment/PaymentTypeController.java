133a4549f3046c4181fca34e9287a0109c7c99aa
free <hunter.com>
2025-01-19 15:36:11 +0800

    公寓管理-支付方式

--- web/web-admin/src/main/java/com/free/lease/web/admin/controller/apartment/PaymentTypeController.java
+++ web/web-admin/src/main/java/com/free/lease/web/admin/controller/apartment/PaymentTypeController.java
@@ -3,8 +3,10 @@ package com.free.lease.web.admin.controller.apartment;
 
 import com.free.lease.common.result.Result;
 import com.free.lease.model.entity.PaymentType;
+import com.free.lease.web.admin.service.PaymentTypeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,22 +16,27 @@ import java.util.List;
 @RequestMapping("/admin/payment")
 @RestController
 public class PaymentTypeController {
+    @Autowired
+    private PaymentTypeService service;
 
     @Operation(summary = "查询全部支付方式列表")
     @GetMapping("list")
     public Result<List<PaymentType>> listPaymentType() {
-        return Result.ok();
+        List<PaymentType> list = service.list();
+        return Result.ok(list);
     }
 
     @Operation(summary = "保存或更新支付方式")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdatePaymentType(@RequestBody PaymentType paymentType) {
+        service.saveOrUpdate(paymentType);
         return Result.ok();
     }
 
     @Operation(summary = "根据ID删除支付方式")
     @DeleteMapping("deleteById")
     public Result deletePaymentById(@RequestParam Long id) {
+        service.removeById(id);
         return Result.ok();
     }
 

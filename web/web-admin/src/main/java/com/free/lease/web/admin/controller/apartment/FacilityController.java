d521dda0cd08f5b41f3fca6605ed3f9486039ffc
free <hunter.com>
2025-01-21 21:32:03 +0800

    公寓管理-配套管理

--- web/web-admin/src/main/java/com/free/lease/web/admin/controller/apartment/FacilityController.java
+++ web/web-admin/src/main/java/com/free/lease/web/admin/controller/apartment/FacilityController.java
@@ -1,11 +1,14 @@
 package com.free.lease.web.admin.controller.apartment;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.free.lease.common.result.Result;
 import com.free.lease.model.entity.FacilityInfo;
 import com.free.lease.model.enums.ItemType;
+import com.free.lease.web.admin.service.FacilityInfoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,22 +18,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/facility")
 public class FacilityController {
+    @Autowired
+    private FacilityInfoService service;
 
     @Operation(summary = "[根据类型]查询配套信息列表")
     @GetMapping("list")
     public Result<List<FacilityInfo>> listFacility(@RequestParam(required = false) ItemType type) {
-        return Result.ok();
+        LambdaQueryWrapper<FacilityInfo> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(type!=null,FacilityInfo::getType,type);
+
+        List<FacilityInfo> list = service.list(queryWrapper);
+        return Result.ok(list);
     }
 
     @Operation(summary = "新增或修改配套信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody FacilityInfo facilityInfo) {
+        service.saveOrUpdate(facilityInfo);
         return Result.ok();
     }
 
     @Operation(summary = "根据id删除配套信息")
     @DeleteMapping("deleteById")
     public Result removeFacilityById(@RequestParam Long id) {
+        service.removeById(id);
         return Result.ok();
     }
 

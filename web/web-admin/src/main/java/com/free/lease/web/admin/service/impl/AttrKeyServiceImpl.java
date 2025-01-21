702edf4c1708e5c9c48f537a2fcd17232d7141ac
free <hunter.com>
2025-01-21 22:26:52 +0800

    公寓管理-基本属性。包含多表连接查询。

--- web/web-admin/src/main/java/com/free/lease/web/admin/service/impl/AttrKeyServiceImpl.java
+++ web/web-admin/src/main/java/com/free/lease/web/admin/service/impl/AttrKeyServiceImpl.java
@@ -4,7 +4,11 @@ import com.free.lease.model.entity.AttrKey;
 import com.free.lease.web.admin.mapper.AttrKeyMapper;
 import com.free.lease.web.admin.service.AttrKeyService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.free.lease.web.admin.vo.attr.AttrKeyVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
 * @author liubo
@@ -15,6 +19,12 @@ import org.springframework.stereotype.Service;
 public class AttrKeyServiceImpl extends ServiceImpl<AttrKeyMapper, AttrKey>
     implements AttrKeyService {
 
+    @Autowired
+    private AttrKeyMapper attrKeyMapper;
+    @Override
+    public List<AttrKeyVo> listAttrInfo() {
+        return attrKeyMapper.listAttrInfo();
+    }
 }
 
 
